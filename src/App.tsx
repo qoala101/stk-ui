760effcbbe7239d5239348a6d3a398db54825654
Volodymyr Hromakov <4y5t6r@gmail.com>
2022-05-27 22:44:51 +0300

    Added Dashboard template from MUI. Removed all test-related stuff.

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type AppState = {
+  public_url: string;
+};
+
+class App extends React.Component<{}, AppState> {
+  state: AppState = {
+    public_url: ""
+  }
+
+  componentDidMount() {
+    this.setState({ public_url: "asd" });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit1 <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <p>{this.state.public_url}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
